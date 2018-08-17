f02028eb97200d5199851428a8430aa5c8260ce7
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-17 17:03:49 +0600

    Complete insert data part

--- runapp.py
+++ runapp.py
@@ -1,20 +1,31 @@
 import sqlite3 as sql
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
- 
-@app.route("/")
-@app.route("/index")
-@app.route("/index.html")
-@app.route("/index.php")
-def index():
+
+def todolists():
 	con = sql.connect("database.db")
 	con.row_factory = sql.Row
 	cur = con.cursor()
 	cur.execute("select * from todos")
-	rows = cur.fetchall()
-	return render_template('index.html',rows = rows)
-	
+	return cur.fetchall()
+
+@app.route("/",methods = ['POST', 'GET'])
+@app.route("/index",methods = ['POST', 'GET'])
+@app.route("/index.html",methods = ['POST', 'GET'])
+def index():
+	if request.method == 'POST':
+		try:
+			task = request.form['todo']
+			with sql.connect("database.db") as con:
+				cur = con.cursor()
+				cur.execute("INSERT INTO todos (task) VALUES (?)",(task,))
+				con.commit()
+		except Exception as e:
+			raise e
+		return render_template('index.html',rows = todolists())
+	else:
+		return render_template('index.html',rows = todolists())
 
 @app.route("/update")
 @app.route("/update.html")
@@ -29,16 +40,6 @@ def about():
 def page_not_found(e):
     return render_template('404.html')
 
-@app.route('/list')
-def list():
-   con = sql.connect("database.db")
-   con.row_factory = sql.Row
-   
-   cur = con.cursor()
-   cur.execute("select * from todos")
-   rows = cur.fetchall();
-   print rows[0]["task"]
-   return rows[0]["task"]
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port= 8080)
