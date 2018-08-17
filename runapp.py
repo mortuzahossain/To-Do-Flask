1c3ff4abade7ad9d16a350ca3ffbaa8606e9d45b
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-17 15:29:11 +0600

    Added All routs

--- runapp.py
+++ runapp.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
  
 @app.route("/")
 @app.route("/index")
-def hello():
-    return "Index"
+@app.route("/index.html")
+@app.route("/index.php")
+def index():
+    return render_template('index.html')
+
+@app.route("/update")
+@app.route("/update.html")
+def update():
+    return render_template('update.html')
 
 @app.route("/about")
 def about():
     return "About"
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port= 8080)
